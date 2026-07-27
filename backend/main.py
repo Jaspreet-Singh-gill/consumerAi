b89cffa97832726ef6e55f4b27d26092e41e6017
jaspreet singh <jaspreetsinghdall@gmail.com>
2026-07-27 15:16:59 +0530

    make the server to run on render

--- backend/main.py
+++ backend/main.py
@@ -33,4 +33,5 @@ def read_root():
     }
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("backend.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
